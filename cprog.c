c64cfe44b3100eba354ef9bef00c6c2a24c1d983
Nikolaev Dmitriy <Igra.nikolaev@gmail.com>
2026-04-06 15:46:30 +0300

    Update cprog.c

--- cprog.c
+++ cprog.c
@@ -4,6 +4,6 @@ extern void my_printf(const char* string, ...);
 
 int main() {
     int h = 987;
-    my_printf("%x\n", h) //output will be : 3DB
+    my_printf("%x\n", h); //output will be : 3DB
     return 0;
 }
